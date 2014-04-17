d6f5e606624819e0031a7ec5da9ce8eac2153f7a
k300021 <k300021@yahoo.com.tw>
2014-04-17 15:21:35 +0800

    a new datastruct Island is creat

---
+++ Island.java
@@ -0,0 +1,14 @@
+package net.floodlightcontroller.netmerging;
+
+import java.util.ArrayList;
+
+import net.floodlightcontroller.core.IOFSwitch;
+
+public class Island {
+	public ArrayList<NetworkUtility> island;
+	
+	Island(){
+		island = new ArrayList<NetworkUtility> ();
+		
+	}
+}
